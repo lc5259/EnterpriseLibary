b91071bdf54b676c35fd3dec7e2bcc6eab23160b
chuang.liu <chuang.liu@LAPTOP-1DERTSDM>
2023-10-14 15:17:16 +0800

    添加项目文件。

---
+++ TestEnterpriseLibary/Program.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace TestEnterpriseLibary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string conStr = "Microsoft.Practices.EnterpriseLibrary.Data.Configuration.DatabaseSettings,Microsoft.Practices.EnterpriseLibrary.Data, Version=5.0.414.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
+            //string cinstr1 = "Server=LAPTOP-1DERTSDM\\MSSQLSERVER2019;Database=[test];Trusted_Connection=True";
+            //Database db = DatabaseFactory.CreateDatabase(cinstr1);
+            //int count = (int)db.ExecuteScalar(CommandType.Text, "SELECT Count(*) From cms_company");
+            //string message = string.Format("There are {0} customers in the database", count.ToString());
+            //Console.WriteLine(message);
+            //Console.ReadLine();
+
+
+            //string sqlConnStr = "Server=LAPTOP-1DERTSDM\\MSSQLSERVER2019;Database=test;Trusted_Connection=True";
+            //SqlConnection sqlConnection = new SqlConnection(sqlConnStr);
+            //try
+            //{
+            //    sqlConnection.Open();
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.Message);
+            //}
+            //string sql = "SELECT * FROM [test].[dbo].[Student]";
+            //SqlCommand cmd = new SqlCommand();//创建SQL语句处理对象
+            //                                  //或SqlCommand cmd = new SqlCommand(sql,sqlConnection);//构造函数绑定
+            //cmd.Connection = sqlConnection;//绑定连接
+            //cmd.CommandText = sql;//绑定SQL语句
+            //int count = (int)cmd.ExecuteScalar();
+
+
+
+            //string conStr = "Microsoft.Practices.EnterpriseLibrary.Data.Configuration.DatabaseSettings,Microsoft.Practices.EnterpriseLibrary.Data, Version=5.0.414.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
+            //string constr1 = "SqlServer.ConnectionString";
+            string str2 = "SqlServer";
+            Database db = DatabaseFactory.CreateDatabase(str2);
+            int count = (int)db.ExecuteScalar(CommandType.Text, "SELECT * FROM [test].[dbo].[Student]");
+
+            string message = string.Format("There are {0} customers in the database", count.ToString());
+            Console.WriteLine(message);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ test2/Program.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Practices.EnterpriseLibrary.Data;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string conStr = "Microsoft.Practices.EnterpriseLibrary.Data.Configuration.DatabaseSettings,Microsoft.Practices.EnterpriseLibrary.Data, Version=5.0.414.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35";
+            string constr1 = "SqlServer.ConnectionString";
+            string str2 = "SqlServer";
+            string str3 = "SqlserverODBC";
+            Database db = DatabaseFactory.CreateDatabase(str3);
+
+            //string dbStr = string.Format("update Student  set name = '{1}', address ='{2}' where id = {0}",
+            //    db.BuildParameterName(":id"),
+            //    db.BuildParameterName("@name"),
+            //    db.BuildParameterName("@address")
+            //    );
+            //DbCommand cmd = db.GetSqlStringCommand(dbStr);
+            //db.AddInParameter(cmd, "id", DbType.Int32,1);
+            //db.AddInParameter(cmd, "name", DbType.String, "mike");
+            //db.AddInParameter(cmd, "address", DbType.String,"上海");
+            //int count = (int)db.ExecuteScalar(cmd);
+            //int count = (int)db.ExecuteScalar(CommandType.Text, "SELECT * FROM [test].[dbo].[Student]");
+
+
+            //and t.id = { 0}
+            string dbStr = string.Format(" SELECT * FROM [test].[dbo].[Teacher] t  where t.id = {0};",
+                db.BuildParameterName("@id")
+                //db.BuildParameterName("@name")
+
+                );
+            DbCommand cmd = db.GetSqlStringCommand(dbStr);
+            db.AddInParameter(cmd, "id", DbType.String, '1');
+            //db.AddInParameter(cmd, "name", DbType.String, "gasg");
+            object sgah = db.ExecuteScalar(cmd);
+            //int count = (int)db.ExecuteScalar(cmd);
+
+
+            //string message = string.Format("There are {0} customers in the database", count.ToString());
+            Console.WriteLine("");
+            Console.ReadLine();
+        }
+    }
+}
